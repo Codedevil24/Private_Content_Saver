809e0eda51d24a3723ee851df2adfc6ac08420f4
Codedevil24 <vishalbhau099@gmail.com>
2025-09-07 23:17:54 +0530

    config.py

--- config.py
+++ config.py
@@ -13,10 +13,10 @@ YTUB_COOKIES = """
 # write here yt cookies
 """
 
-API_ID = int(getenv("API_ID", "23057509"))
-API_HASH = getenv("API_HASH", "a1ba4eb481c7333fc8822de292e46f5e")
+API_ID = int(getenv("API_ID", "23677130"))
+API_HASH = getenv("API_HASH", "805dcfa1dca9bf80f70a571bf6deb7c0")
 BOT_TOKEN = getenv("BOT_TOKEN", "7766703530:AAHyfFPQXxX8sMa5faLppqcfbjFhjxc5QKQ")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "5083822493").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "6859104243").split()))
 MONGO_DB = getenv("MONGO_DB", "mongodb+srv://devv54123:sz7hqWLr9cNzN5JG@cluster0.ahus5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 LOG_GROUP = getenv("LOG_GROUP", "-1002828779986")
 CHANNEL_ID = int(getenv("CHANNEL_ID", "-1001579030541"))
@@ -25,6 +25,6 @@ PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "500"))
 WEBSITE_URL = getenv("WEBSITE_URL", "gplink.com")
 AD_API = getenv("AD_API", "e1b77a06fabdc3b60c575020fc9382fd33bedc39")
 STRING = getenv("STRING", None)
-YT_COOKIES = getenv("YT_COOKIES", YTUB_COOKIES)
+YT_COOKIES = getenv("YT_COOKIES", None)
 DEFAULT_SESSION = getenv("DEFAUL_SESSION", None)  # added old method of invite link joining
-INSTA_COOKIES = getenv("INSTA_COOKIES", INST_COOKIES)
+INSTA_COOKIES = getenv("INSTA_COOKIES", None)
